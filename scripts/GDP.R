359da4fc9c89c9693a754d78e98efc12f0c012ab
Anna Bajema <anna.bajema@gmail.com>
2015-10-02 15:27:48 +1000

    sdjfajsf

--- scripts/GDP.R
+++ scripts/GDP.R
@@ -9,3 +9,4 @@ CalGDP <- function(dat){ #define funtion name <- function(arbitary parameter nam
   return(GDP)# instructing the funtion to return the result - do not need this, but need it if you want the funtion to provide an output
 
 CalGDP(gapminder)# call to the funtion. Put in brackets what information is needed to run the funtion. This can be a signle digit (as in example on BasicFunctions script) or it can be referenceing to a data set (as used in this function)
+#ajsdfhasfhashfashfa
